14f98c9faab2c2ff108b645a58b1510acaa32388
Wilbert <wilbert.caballero@aptiv.com>
2020-11-06 15:20:24 -0600

    added gitignore and updated project to relative paths

--- code/libs/lpc_chip_43xx/Debug/src/ring_buffer.d
+++
@@ -1,7 +0,0 @@
-src/ring_buffer.o src/ring_buffer.d: ../src/ring_buffer.c \
- /home/main/Nas/will/06_WK/git/LPCXpresso4367_USB/code/libs/lpc_chip_43xx/inc/ring_buffer.h \
- /home/main/Nas/will/06_WK/git/LPCXpresso4367_USB/code/libs/lpc_chip_43xx/inc/lpc_types.h
-
-/home/main/Nas/will/06_WK/git/LPCXpresso4367_USB/code/libs/lpc_chip_43xx/inc/ring_buffer.h:
-
-/home/main/Nas/will/06_WK/git/LPCXpresso4367_USB/code/libs/lpc_chip_43xx/inc/lpc_types.h:
--- code/libs/lpc_chip_43xx_m0/Debug/src/ring_buffer.d
+++
@@ -1,7 +0,0 @@
-src/ring_buffer.o src/ring_buffer.d: ../src/ring_buffer.c \
- /home/main/lpcopen_3_02_lpcxpresso_xpresso4337/lpc_chip_43xx_m0/inc/ring_buffer.h \
- /home/main/lpcopen_3_02_lpcxpresso_xpresso4337/lpc_chip_43xx_m0/inc/lpc_types.h
-
-/home/main/lpcopen_3_02_lpcxpresso_xpresso4337/lpc_chip_43xx_m0/inc/ring_buffer.h:
-
-/home/main/lpcopen_3_02_lpcxpresso_xpresso4337/lpc_chip_43xx_m0/inc/lpc_types.h:
